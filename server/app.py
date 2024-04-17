0ed0715e781a54b5b01e82b45f52b18df58ae6c0
Koby Fink <finkkoby@icloud.com>
2024-04-17 19:13:04 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -24,6 +24,53 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+class Restaurants(Resource):
+    def get(self):
+        restaurants = [restaurant.to_dict(only=('id', 'address', 'name'))
+                       for restaurant in Restaurant.query.all()]
+        return restaurants, 200
+    
+class RestaurantsId(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+        if restaurant:
+            return restaurant.to_dict(), 200
+        else:
+            return {"error": "Restaurant not found"}, 404
+    def delete(self, id):
+        restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+            return {}, 204
+        else:
+            return {"error": "Restaurant not found"}, 404
+        
+class Pizzas(Resource):
+    def get(self):
+        pizzas = [pizza.to_dict(only=('id', 'name', 'ingredients'))
+                  for pizza in Pizza.query.all()]
+        return pizzas, 200
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        json = request.get_json()
+        try:
+            restaurant_pizza = RestaurantPizza(
+                price=json['price'],
+                pizza_id=json['pizza_id'],
+                restaurant_id=json['restaurant_id']
+            )
+            db.session.add(restaurant_pizza)
+            db.session.commit()
+            return restaurant_pizza.to_dict(), 201
+        except:
+            return {"errors": ["validation errors"]}, 400
+
+api.add_resource(Restaurants, '/restaurants')
+api.add_resource(RestaurantsId, '/restaurants/<int:id>')
+api.add_resource(Pizzas, '/pizzas')
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
